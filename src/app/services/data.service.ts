c9674502fd0d4e58ac40d0aa9309cf299da840a5
iamjoaohenrique <joaohs50@gmail.com>
2020-10-07 14:28:35 -0300

    alterações no password e url da api nova

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,8 +8,9 @@ import { debounceTime } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DataService {
-  private readonly API_URL = 'https://api.github.com/repos/angular/angular/issues';
+  //private readonly API_URL = 'https://api.github.com/repos/angular/angular/issues';
   // private readonly API_URL = 'http://138.197.225.217:8080/profiles';
+  private readonly API_URL ='http://register-profile.qa.appmobbuy.tech:8080/profiles';
 
 
   dataChange: BehaviorSubject<Profile[]> = new BehaviorSubject<Profile[]>([]);
