2a758fa5da9dbf9d4d9a5767442a47d2525c5f2f
iamjoaohenrique <joaohs50@gmail.com>
2020-10-07 17:21:36 -0300

    correção do caminho dos dados da api que iram exibir no data table do grupo de usuarios

--- src/app/models/profile.ts
+++ src/app/models/profile.ts
@@ -1,7 +1,34 @@
 export class Profile {
-  id: number;
-  title: string;
-  nameprofile: string;
+  content: Content[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  sort: Sort;
+  number: number;
+  numberOfElements: number;
+  first: boolean;
+  size: number;
+  empty: boolean;
+}
+
+interface Pageable {
+  sort: Sort;
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+interface Sort {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+interface Content {
+  idProfile: number;
+  nameProfile: string;
   description: string;
-  situation: string;
 }
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Profile } from '../../models/profile';
 import { ActionModel } from 'src/app/@core/models/action.model';
 import { HeaderModel } from 'src/app/@core/models/header.model';
 import { DataService } from 'src/app/services/data.service';
@@ -20,10 +21,9 @@ export class UserComponent implements OnInit {
     this.loadData();
   }
   headers: HeaderModel[] = [
-    { text: 'Código', value: 'id' },
+    { text: 'Código', value: 'idProfile' },
     { text: 'Empresa', value: 'title' },
-    { text: 'Identificação', value: 'nameprofile' },
-    { text: 'Código', value: 'description' },
+    { text: 'Descrição', value: 'description' },
     { text: 'Situação', value: 'situation' },
     // { text: 'Ações', value: 'action' }
   ];
