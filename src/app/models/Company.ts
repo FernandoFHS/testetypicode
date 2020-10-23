89be2ebd75d78b94b3e68b81701a9e90bb90d0cf
VitorPinheiro29 <vitorca.pinheiro@gmail.com>
2020-10-23 11:16:08 -0300

    adicionando formbuilder para contas bancárias

---
+++ src/app/models/Company.ts
@@ -0,0 +1,75 @@
+export class Mcc {
+  id: number;
+  description: string;
+}
+
+export class Cnae {
+  idCnae: number;
+  mcc: Mcc;
+  description: string;
+  descGroup: string;
+}
+
+export class CompanyContent {
+  idCompany: number;
+  companyAddress: any[];
+  companyContact: any[];
+  migration?: any;
+  cnae: Cnae;
+  email?: any;
+  registrationDate?: any;
+  gpSendDate?: any;
+  accreditationDate: string;
+  discreditationDate?: any;
+  salesTableNumber?: any;
+  companyStatus?: any;
+  userInclusionCode?: any;
+  inclusionRegistrationDateTime?: any;
+  userChangeCode?: any;
+  recordChangeDateTime?: any;
+  automaticCreditIndicator?: any;
+  seRegistrationDate?: any;
+  referentialTransactionAmount?: any;
+  posBillingTypeRental?: any;
+  posChargeAmountRental?: any;
+  tradingPartnerCode?: any;
+  idTerminal?: any;
+  logicalNumber?: any;
+  gpAffiliationDate?: any;
+  rentalExemptionDays?: any;
+  registerCode?: any;
+  posPercentageRateValue?: any;
+  anticipationFee?: any;
+  orderType?: any;
+  situation?: any;
+  searchNickname?: any;
+  automaticAnticipationIndicator?: any;
+  documentNumberCompany: string;
+  companyType?: any;
+  companyName?: any;
+  fancyName?: any;
+  stateRegistration?: any;
+  businessActivity?: any;
+  estUrl?: any;
+  companyResponsibleName?: any;
+  openingHours?: any;
+  openingDate?: any;
+  shopping?: any;
+  referencePoint?: any;
+  ecommerceURL?: any;
+  idCompanyGroup: number;
+  idDepartament?: any;
+  equipmentIdentifier?: any;
+  posQuantity?: any;
+  gpEstablishmentNumber?: any;
+  companyShortName?: any;
+  idCompanyOwner?: any;
+  transactionAmount?: any;
+  tedBillingIdentifier?: any;
+  tedAmount?: any;
+  identificationWithSoftwareExpress?: any;
+}
+
+export class RootObject {
+  content: CompanyContent[];
+}
---
+++ src/app/services/company.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Observable, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CompanyComponent } from '../pages/company/company.component';
+import { CompanyContent, Mcc, RootObject } from '../models/Company';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  private _refreshTable = new Subject<void>();
+
+  private readonly API_URL = 'http://bffmaintenance.qa.appmobbuy.tech:8080/company';
+
+  constructor(private httpClient: HttpClient, public _snackBar: MatSnackBar) { }
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  /** CRUD METHODS */
+  getAllProfiles(): Promise<CompanyContent[]> {
+    return new Promise<CompanyContent[]>((resolve, reject) => {
+      this.httpClient.get<CompanyContent[]>(this.API_URL).subscribe(
+        (data) => {
+          resolve(data);
+          // this.dataChange.next(data);
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error.name + ' ' + error.message);
+          reject(error);
+        }
+      );
+    });
+  }
+
+  create(company: CompanyContent): Observable<CompanyContent> {
+    return this.httpClient.post<CompanyContent>(this.API_URL, company).pipe(
+      tap(() => {
+        this._refreshTable.next();
+      })
+    );
+  }
+
+  readById(idCompany: number): Observable<CompanyContent> {
+    const url = `${this.API_URL}/${idCompany}`;
+    return this.httpClient.get<CompanyContent>(url);
+  }
+
+  update(company: CompanyContent): Observable<CompanyContent> {
+    const url = `${this.API_URL}/${company.idCompany}`;
+    return this.httpClient.put<CompanyContent>(url, company).pipe(
+      tap(() => {
+        this._refreshTable.next();
+      })
+    );;
+  }
+
+  delete(idCompany: number): Observable<CompanyContent> {
+    const url = `${this.API_URL}/${idCompany}`;
+    return this.httpClient.delete<CompanyContent>(url).pipe(
+      tap(() => {
+        this._refreshTable.next();
+      })
+    );;
+    //this.getAllProfiles();
+  }
+
+  openSnackBar(message: string, action: string): void {
+    this._snackBar.open(message, action, {
+      duration: 3000,
+      panelClass: ['mat-snack-success'],
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+}
