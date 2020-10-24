5fb604f7e8c2dab34ac0e68097b92e26da43f492
VitorPinheiro29 <vitorca.pinheiro@gmail.com>
2020-10-24 10:27:43 -0300

    adicionando tela de planos na sidebar

--- src/app/@core/consts/menu-items.ts
+++ src/app/@core/consts/menu-items.ts
@@ -5,5 +5,6 @@ export const menuItems: Menu[] = [
   { route: 'profile-list', name: 'Grupo de Usuários', type: 'link', icon: 'group' },
   { route: 'company-list', name: 'Estabelecimento', type: 'link', icon: 'house' },
   { route: 'password-transaction', name: 'Senha Transação', type: 'link', icon: 'vpn_key' },
-  { route: 'login', name: 'Login', type: 'link', icon: 'lock' }
+  { route: 'login', name: 'Login', type: 'link', icon: 'lock' },
+  { route: 'login', name: 'Planos', type: 'link', icon: 'lock' }
 ]
