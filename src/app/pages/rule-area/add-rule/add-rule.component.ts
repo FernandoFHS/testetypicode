784217cc03ec77f9903e78907408f63384bfad8c
keltecnologia <joaopedrocosta.contato@gmail.com>
2020-11-04 10:47:25 -0300

    Desenvolvimento do cadastro de Regras

--- src/app/pages/rule-area/add-rule/add-rule.component.ts
+++ src/app/pages/rule-area/add-rule/add-rule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 import { BreadcrumbModel } from 'src/app/@core/models/breadcrumb';
 
 @Component({
@@ -21,7 +22,143 @@ export class AddRuleComponent implements OnInit {
   };
 
   form: FormGroup;
-  form_conditions;
+
+  logic_operator_list: any[] = [{
+    text: 'E',
+    value: 1
+  }, {
+    text: 'OU',
+    value: 2
+  }];
+
+  variable_list: any[] = [{
+    text: 'Valor Transação',
+    value: 0,
+    comparison_operator_list: [
+      { text: 'Igual' },
+      { text: 'Maior' },
+      { text: 'Menor' },
+      { text: 'Maior ou Igual' },
+      { text: 'Menor ou Igual' },
+      { text: 'Diferente' },
+    ]
+  }, {
+    text: 'Adquirente',
+    value: 1,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'CNAE',
+    value: 2,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'MCC',
+    value: 3,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'Modo Entrada',
+    value: 4,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'Tipo Venda',
+    value: 5,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'Status Transação',
+    value: 6,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'Código Retorno',
+    value: 7,
+    comparison_operator_list: [
+      { text: 'Entre' },
+      { text: 'Diferente' },
+      { text: 'Entre' },
+    ]
+  }, {
+    text: 'Horário Transação',
+    value: 8,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Menor' },
+      { text: 'Maior ou Igual' },
+      { text: 'Menor ou Igual' },
+    ]
+  }, {
+    text: 'Recorrência - Mesmo Estabelecimento e Mesmo Valor Transação',
+    value: 9,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Recorrência - Mesmo Estabelecimento, Mesmo Valor Transação e Mesmo Número Cartão',
+    value: 10,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Recorrência - Mesmo Estabelecimento e Mesmo Número Cartão',
+    value: 11,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Volume',
+    value: 12,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Volume por Hora',
+    value: 13,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Ticket Médio',
+    value: 14,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }, {
+    text: 'Ticket Médio por Hora',
+    value: 15,
+    comparison_operator_list: [
+      { text: 'Maior' },
+      { text: 'Maior ou Igual' }
+    ]
+  }];
+
+  condition_comparison_operator_list: any[] = [];
 
   constructor(
     private _formBuilder: FormBuilder
@@ -31,20 +168,32 @@ export class AddRuleComponent implements OnInit {
     this._loadForm();
   }
 
-  private _loadForm(): void {
-    this.form = this._formBuilder.group({
-      description: ['', [Validators.required]],
-      conditions: new FormArray([
-        new FormGroup({
-          operador_logico: new FormControl([]),
-          variavel: new FormControl([]),
-          op_comparacao: new FormControl([]),
-          valor: new FormControl([])
-        })
-      ])
-    });
-
-    this.form_conditions = this.form.get('conditions') as FormArray;
+  addCondition(): void {
+    const conditions = this.form.get('conditions') as FormArray;
+    conditions.push(this._createCondition());
+    this.condition_comparison_operator_list.push();
   }
 
+  onChangeVariable(event: MatSelectChange, index: number): void {
+    console.log(event);
+    this.condition_comparison_operator_list[index] = event.value;
+
+    console.log(this.condition_comparison_operator_list);
+  }
+
+  private _loadForm(): void {
+    this.form = this._formBuilder.group({
+      description: ['', []],
+      conditions: this._formBuilder.array([])
+    });
+  }
+
+  private _createCondition(): FormGroup {
+    return this._formBuilder.group({
+      operador_logico: [{ value: '', disabled: false }, []],
+      variavel: [{ value: '', disabled: false }, []],
+      op_comparacao: [{ value: '', disabled: false }, []],
+      valor: [{ value: '', disabled: true }, []]
+    });
+  }
 }
