ad2d3f6ae57d7cbe1e985f86b7e815d2e27e6368
Luciano Lima <luciano.lima93@outlook.com.br>
2020-11-10 15:06:30 -0300

    Planos

---
+++ src/app/services/acquirer.service.ts
@@ -0,0 +1,26 @@
+import { AcquirerResponse, AcquirerRequest } from './../models/plans/Acquirer';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcquirerService {
+  private readonly url = `${environment.baseUrl}acquirer`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<AcquirerResponse>(this.url)
+  }
+  getById(id:number) {
+    return this.http.get<AcquirerRequest>(`${this.url}/${id}`)
+  }
+  postOrPut(acquirer: AcquirerRequest) {
+    return this.http.post<AcquirerRequest>(this.url, acquirer)
+  }
+  delete(id:number) {
+    return this.http.get<boolean>(`${this.url}/${id}`)
+  }
+}
