a152251ba464d75156ddbe3a356208349caba371
iamjoaohenrique <joaohs50@gmail.com>
2020-11-24 14:44:51 -0300

    atualizoes company

--- src/app/pages/companies/partners/add/add-partner.component.ts
+++ src/app/pages/companies/partners/add/add-partner.component.ts
@@ -6,19 +6,43 @@ import { DataService } from 'src/app/services/data.service';
 import { CepService } from 'src/app/services/cep.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { BreadcrumbModel } from 'src/app/@core/models/breadcrumb';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
 
 @Component({
   selector: 'app-add-partner',
   templateUrl: './add-partner.component.html',
   styleUrls: ['./add-partner.component.scss'],
+  providers: [
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'pt-BR',
+    },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
 })
 export class AddPartnerComponent implements OnInit {
 
   cep: number;
   teste;
   partnerArray: any[];
+  partnerArrayEdit: any[];			  
 
   partnerSource: any = this.localStorageService.get('partnerFormGroup');
+  partnerSourceEdit: any = this.localStorageService.get('editPartner');
   response;
   partner: any;
   index: any;
@@ -33,7 +57,7 @@ export class AddPartnerComponent implements OnInit {
     },
     items: [
       { title: 'Home', route: '' },
-      { title: 'Lista de Estabelecimentos', route: 'company-list' },
+      { title: 'Lista de Estabelecimentos', route: 'companies/list' },
       { title: 'Incluir Estabelecimento', route: 'companies/add' },
     ]
   };
@@ -63,42 +87,42 @@ export class AddPartnerComponent implements OnInit {
   ngOnInit(): void {
     this.loadParams().then(() => {
       this.partnerFormGroup = this._formBuilder.group({
-        sequenceNumber: [{ value: '', disabled: true }, Validators.required],
-        name: [this.partner?.name || '', Validators.required],
+        partnerSequentialNumber: [{ value: '', disabled: true }, Validators.required],
+        partnerName: [this.partner?.partnerName || '', Validators.required],
         cpf: [this.partner?.cpf || '', Validators.required],
         dateOfBirth: [this.partner?.dateOfBirth || '', Validators.required],
-        cep: [this.partner?.cep || '', Validators.required],
-        street: [this.partner?.street || '', Validators.required],
+        zipCode: [this.partner?.zipCode || '', Validators.required],
+        streetName: [this.partner?.streetName || '', Validators.required],
         number: [this.partner?.number || '', Validators.required],
         complement: [this.partner?.complement || ''],
-        neighborhood: [this.partner?.neighborhood || '', Validators.required],
-        county: [this.partner?.county || '', Validators.required],
-        state: [this.partner?.state || '', Validators.required],
-        contact: [this.partner?.contact || '', Validators.required]
+        neighborhoodName: [this.partner?.neighborhoodName || '', Validators.required],
+        cityName: [this.partner?.cityName || '', Validators.required],
+        stateName: [this.partner?.stateName || '', Validators.required],
+        phone: [this.partner?.phone || '', Validators.required]
       });
     });
 
     if (!this.index) {
       this.addPage = true;
-
-      this.partnerFormGroup = this._formBuilder.group({
-        sequenceNumber: [{ value: '', disabled: true }, Validators.required],
-        name: ['', Validators.required],
-        cpf: ['', Validators.required],
-        dateOfBirth: ['', Validators.required],
-        cep: ['', Validators.required],
-        street: ['', Validators.required],
-        number: ['', Validators.required],
-        complement: [''],
-        neighborhood: ['', Validators.required],
-        county: ['', Validators.required],
-        state: ['', Validators.required],
-        contact: ['', Validators.required]
-      });
+      this.partnerFormGroup = new FormGroup({
+      cpf: new FormControl(''),
+      dateOfBirth: new FormControl(''),
+      cityName: new FormControl(''),
+      neighborhoodName: new FormControl(''),
+      stateName: new FormControl(''),
+      streetName: new FormControl(''),
+      complement: new FormControl(''),
+      number: new FormControl(''),
+      phone: new FormControl(''),
+      zipCode: new FormControl(''),
+      partnerName: new FormControl(''),
+      partnerSequentialNumber: new FormControl(''),
+		 
+    });
     } else {
       this.addPage = false;
     }
-    
+    console.log(this.addPage);
   }
 
   formControl = new FormControl('', [
@@ -126,7 +150,7 @@ export class AddPartnerComponent implements OnInit {
 
           if (typeof (index) == 'string') {
 
-            const partnerArray = this.localStorageService.get('partnerFormGroup');
+            const partnerArray = this.localStorageService.get('editPartner');
 
             if (partnerArray && partnerArray.length > 0) {
               const partner = partnerArray[index];
@@ -150,16 +174,14 @@ export class AddPartnerComponent implements OnInit {
   }
 
   getAdressByCep(value) {
-    this.cep = value;
-    console.log(this.cep);
-    this.CepService.getCep(this.cep).subscribe((response: any) => {
+   this.CepService.getCep(value).subscribe((response: any) => {
       console.log(response);
 
       let obj = {
-        county: response.localidade,
-        street: response.logradouro,
-        neighborhood: response.bairro,
-        state: response.uf,
+        cityName: response.localidade,
+        streetName: response.logradouro,
+        neighborhoodName: response.bairro,
+        stateName: response.uf,
       };
       this.teste = response;
       this.partnerFormGroup.patchValue(obj);
@@ -168,12 +190,119 @@ export class AddPartnerComponent implements OnInit {
   }
 
   savePartner(form) {
+    const form1  = this.partnerFormGroup.getRawValue();
+    this.partner =  {
+      cpf : form1.cpf,
+      dateOfBirth: "2020-11-19T19:21:08.349Z",
+      partnerName: this.partnerFormGroup.get('partnerName').value,
+      partnerAddress:[{
+        complement:this.partnerFormGroup.get('complement').value,
+        number:this.partnerFormGroup.get('number').value,
+        street:{
+          city:{
+            cityName: this.partnerFormGroup.get('cityName').value,
+          },
+          neighborhood:{
+            neighborhoodName:this.partnerFormGroup.get('neighborhoodName').value,
+          },
+          state:{
+            stateName:this.partnerFormGroup.get('stateName').value.toLowerCase(),
+            uf: this.partnerFormGroup.get('stateName').value.toLowerCase()
+          },
+          streetName: this.partnerFormGroup.get('streetName').value,
+          zipCode: this.partnerFormGroup.get('zipCode').value,
+        }
+      }],
+      partnerContact: [
+        {
+          phone: parseInt(this.partnerFormGroup.get('phone').value),
+        }
+      ],
+      partnerSequentialNumber: 1
+      
+    }
+    console.log(this.partner);
+
+    let partnerArrayEdit = this.localStorageService.get('editPartner');
+    if (!partnerArrayEdit) {
+      partnerArrayEdit = [];
+    }
+
     let partnerArray = this.localStorageService.get('partnerFormGroup');
     if (!partnerArray) {
       partnerArray = [];
     }
-    partnerArray.push(form.value);
+    partnerArrayEdit.push(form.value);
+    this.localStorageService.set('editPartner', partnerArrayEdit);
+
+    partnerArray.push(this.partner);
     this.localStorageService.set('partnerFormGroup', partnerArray);
+
+    this.router.navigate(['/companies/add']);
+  }
+
+  editPartner() {
+    let index = this.index;
+
+    let editable = {
+      cpf: this.partnerFormGroup.get('cpf').value,
+      dateOfBirth: this.partnerFormGroup.get('dateOfBirth').value,
+      cityName: this.partnerFormGroup.get('cityName').value,
+      neighborhoodName: this.partnerFormGroup.get('neighborhoodName').value,
+      stateName: this.partnerFormGroup.get('stateName').value,
+      streetName: this.partnerFormGroup.get('streetName').value,
+      complement: this.partnerFormGroup.get('complement').value,
+      number: this.partnerFormGroup.get('number').value,
+      phone: parseInt(this.partnerFormGroup.get('phone').value),
+      zipCode: this.partnerFormGroup.get('zipCode').value,
+      partnerName: this.partnerFormGroup.get('partnerName').value,
+      partnerSequentialNumber: 1,
+    }
+
+    let editableItem =  {
+      cpf : this.partnerFormGroup.get('cpf').value,
+      dateOfBirth: this.partnerFormGroup.get('dateOfBirth').value,
+      partnerName: this.partnerFormGroup.get('partnerName').value,
+      partnerAddress:[{
+        complement:this.partnerFormGroup.get('complement').value,
+        number:this.partnerFormGroup.get('number').value,
+        street:{
+          city:{
+            cityName: this.partnerFormGroup.get('cityName').value,
+          },
+          neighborhood:{
+            neighborhoodName:this.partnerFormGroup.get('neighborhoodName').value,
+          },
+          state:{
+            stateName:this.partnerFormGroup.get('stateName').value.toLowerCase(),
+             uf: this.partnerFormGroup.get('stateName').value.toLowerCase()
+          },
+          streetName: this.partnerFormGroup.get('streetName').value,
+          zipCode: this.partnerFormGroup.get('zipCode').value,
+        }
+      }],
+      partnerContact: [
+        {
+          phone: parseInt(this.partnerFormGroup.get('phone').value),
+        }
+      ],
+      partnerSequentialNumber: 1,
+      
+    }
+
+    if (index > -1) {
+      Object.assign(this.partnerSourceEdit[index], editable);
+      localStorage.setItem('editPartner', JSON.stringify(this.partnerSourceEdit));
+    } else {
+      console.log(editable);
+    }
+
+    if (index > -1) {
+      Object.assign(this.partnerSource[index], editableItem);
+      localStorage.setItem('partnerFormGroup', JSON.stringify(this.partnerSource));
+    } else {
+      console.log(editableItem);
+    }
     this.router.navigate(['/companies/add']);
   }
 
