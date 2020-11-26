53b23cb92d452d2e18b6ce5729fcc2060a06614b
João Costa <joaopedrocosta.contato@gmail.com>
2020-11-26 17:16:44 -0300

    Correção do titulo do menu transação

--- src/app/@core/consts/menu-items.ts
+++ src/app/@core/consts/menu-items.ts
@@ -3,9 +3,9 @@ import { Menu } from 'src/app/@core/models/menu.model';
 export const menuItems: Menu[] = [
   { route: 'home', display_name: 'Home', icon_name: 'home', children: [], open: false },
   {
-    route: 'transaction', display_name: 'Transações', icon_name: 'payments', children: [
+    route: 'transaction', display_name: 'Conta Corrente', icon_name: 'payments', children: [
       { route: 'transaction/extract', icon_name: 'receipt_long', display_name: 'Extrato', children: [], open: false },
-      { route: 'transaction/mov', icon_name: 'receipt_long', display_name: 'Movimentação da Conta', children: [], open: false }
+      { route: 'transaction/mov', icon_name: 'history_edu', display_name: 'Lançamentos Futuros', children: [], open: false }
     ], open: false
   },
   { route: 'profile-list', display_name: 'Grupo de Usuários', icon_name: 'group', children: [], open: false },
