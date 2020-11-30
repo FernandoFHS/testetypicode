ca5b11094bf2c68aca5418a1170cc8f5c4ad8f40
iamjoaohenrique <joaohs50@gmail.com>
2020-11-30 15:09:18 -0300

    criação da tela de recuração de senha

--- src/app/pages/password-transaction/recover-password-transaction/recover-password-transaction.component.ts
+++ src/app/pages/password-transaction/recover-password-transaction/recover-password-transaction.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BreadcrumbModel } from 'src/app/@core/models/breadcrumb';
+import { ConfirmedValidator } from 'src/app/@core/validators/confirmed.validator';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-recover-password-transaction',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recover-password-transaction.component.scss']
 })
 export class RecoverPasswordTransactionComponent implements OnInit {
+  recoverPasswordForm: FormGroup;
+  hide1 = true;
 
-  constructor() { }
+  hasPassword: boolean = false;
+
+  //RASCUNHOS PARA O FUTURO
+
+  //password = sessionStorage....
+
+  // hasPassword: boolean;
+  // if (hasPassword) {
+    //hasPassowrd = true;
+  //} 
+
+
+  breadcrumbModel: BreadcrumbModel = {
+    active: {
+      title: 'Senha Transação',
+      route: ''
+    },
+    items: [
+      { title: 'Home', route: '' },
+    ]
+  };
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private router: Router,
+    private _notificationService: NotificationService,
+  ) { }
 
   ngOnInit(): void {
+    this.recoverPasswordForm = this._formBuilder.group({
+      Password: ['', [Validators.required]],
+    })
+  }
+
+  navigateToInitialPassword(): void {
+    this.router.navigate(['/password-transaction/initial'])
+  }
+
+  submit(): void {
+    if (this.recoverPasswordForm.valid) {
+    this._notificationService.success('Foi enviado para o seu e-mail cadastrado um link para redefinição de senha!');
+    }
   }
 
 }
