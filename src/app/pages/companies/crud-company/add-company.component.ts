8a56dae5bed17bd07e0ea304ab995d53557cf44d
iamjoaohenrique <joaohs50@gmail.com>
2020-12-05 16:05:01 -0300

    incluindo a tag do Google Analytics e mudanças

--- src/app/pages/companies/crud-company/add-company.component.ts
+++ src/app/pages/companies/crud-company/add-company.component.ts
@@ -531,7 +531,7 @@ export class AddCompanyComponent implements OnInit {
       externalBankAccount: this._formBuilder.array(this.apiBankAccount$),
     });
 
-    this.partnerFormGroup = this._formBuilder.group({
+    this.companyPartnerFormGroup = this._formBuilder.group({
       companyPartner: this._formBuilder.array(company.companyPartner),
     });
 
