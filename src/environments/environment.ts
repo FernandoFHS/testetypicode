0aa5b01c2c93c9ed8f7a2f39304dd60c5c67e8d6
João Costa <joaopedrocosta.contato@gmail.com>
2020-12-07 19:40:19 -0300

    Alteração da flag de mock do bff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     mock: false
   },
   bff: {
-    mock: true,
+    mock: false,
     url: 'http://company.qa.appmobbuy.tech:8080',
     url_2: 'http://register-plans.qa.appmobbuy.tech:8080',
     url_login: 'http://bffauthentication.qa.appmobbuy.tech:8080/users/login',
